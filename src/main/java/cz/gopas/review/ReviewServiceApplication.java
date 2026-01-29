a4f41542f4ffe0805739c250e775d454e7ea5857
Aleš Roszoha <a.roszoha@gmail.com>
2026-01-29 15:45:13 +0100

    Calling Book service via REST client

--- src/main/java/cz/gopas/review/ReviewServiceApplication.java
+++ src/main/java/cz/gopas/review/ReviewServiceApplication.java
@@ -2,12 +2,19 @@ package cz.gopas.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ReviewServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReviewServiceApplication.class, args);
+    }
+
+    @Bean
+    RestTemplate getBookServiceClient() {
+        return new RestTemplate();
     }
 
 }
--- src/main/java/cz/gopas/review/controller/ReviewControllerV1.java
+++ src/main/java/cz/gopas/review/controller/ReviewControllerV1.java
@@ -2,15 +2,19 @@ package cz.gopas.review.controller;
 
 import cz.gopas.review.bean.Review;
 import cz.gopas.review.persistence.Storage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequestMapping("/v1")
 public class ReviewControllerV1 implements ReviewController {
@@ -18,6 +22,11 @@ public class ReviewControllerV1 implements ReviewController {
     @Autowired
     private Storage storage;
 
+    @Autowired
+    private RestTemplate bookServiceClient;
+
+    @Value("${custom.book-service-base-url}")
+    private String bookServiceBaseURL;
 
     @Override
     public List<Review> getReviews() {
@@ -38,6 +47,8 @@ public class ReviewControllerV1 implements ReviewController {
     @Override
     public ResponseEntity<Review> createReview(Review newReview) {
         try {
+            ResponseEntity<String> serviceResult = bookServiceClient.getForEntity(bookServiceBaseURL + newReview.getBookId(), String.class);
+            log.info(serviceResult.getBody());
             Review result = storage.createBook(newReview);
             return ResponseEntity.status(HttpStatus.CREATED)
                     .header("X-ReviewID", String.valueOf(result.getId()))
